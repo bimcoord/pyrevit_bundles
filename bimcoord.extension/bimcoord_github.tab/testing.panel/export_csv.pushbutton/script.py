cd2a5283940c6e615359ceaf87e0b4888cfcc107
bogdan <bogdan.bimcoord@gmail.com>
2022-09-20 22:16:31 +0300

    22.15

--- bimcoord.extension/bimcoord_github.tab/testing.panel/export_csv.pushbutton/script.py
+++ bimcoord.extension/bimcoord_github.tab/testing.panel/export_csv.pushbutton/script.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 __title__ = 'Export\nCSV'
 __doc__ = '''Define later in bundle.yaml'''
+# Template
+from Autodesk.Revit import DB
+from rpw.ui import forms
 
-#class or script ?
+doc = __revit__.ActiveUIDocument.Document
 
-print("Hello, there!")
+
+
+# Checking document like family or project
+if doc.IsFamilyDocument:
+    forms.Alert('Это семейство', exit=True)
+else:
+    forms.Alert('Это документ', exit=True)
