acea57507d51497f03e745d51c680b0ccbfa9844
bogdan <bogdan.bimcoord@gmail.com>
2022-09-23 15:09:14 +0300

    23.09 15/09

--- bimcoord.extension/bimcoord_github.tab/testing.panel/export_csv.pushbutton/script.py
+++ bimcoord.extension/bimcoord_github.tab/testing.panel/export_csv.pushbutton/script.py
@@ -19,13 +19,15 @@ if doc.IsFamilyDocument:
         list_of_size_tables
     )
     selected_size_table = family_size_table_manager.GetSizeTable(selected_size_table)
-    print(selected_size_table) # Удалить
-    print(selected_size_table.AsValueString(1,1)) # Удалить
 
     #Алгоритм перебора
     # Размер таблицы:
     columns = selected_size_table.NumberOfColumns
     rows = selected_size_table.NumberOfRows
+
+    # Получение шапки таблица. Также под вопросом 
+    # print(columns,rows)
+    # print(selected_size_table.GetColumnHeader(2).GetUnitTypeId()) # Получение информации о типе под вопросом
 
 else:
     forms.Alert('Это документ', exit=True)
