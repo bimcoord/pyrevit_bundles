91e0fc8290331b9fce755a051e5006f427204564
bogdan <bogdan.bimcoord@gmail.com>
2022-09-23 14:45:01 +0300

    23.09 14.44

--- bimcoord.extension/bimcoord_github.tab/testing.panel/export_csv.pushbutton/script.py
+++ bimcoord.extension/bimcoord_github.tab/testing.panel/export_csv.pushbutton/script.py
@@ -7,10 +7,25 @@ from rpw.ui import forms
 
 doc = __revit__.ActiveUIDocument.Document
 
-
-
 # Checking document like family or project
 if doc.IsFamilyDocument:
-    forms.Alert('Это семейство', exit=True)
+    owner_family_id = doc.OwnerFamily.Id
+    family_size_table_manager = DB.FamilySizeTableManager.\
+        GetFamilySizeTableManager(doc, owner_family_id) # Получение менеджера таблиц
+    list_of_size_tables = list(family_size_table_manager.GetAllSizeTableNames())
+    
+    selected_size_table = forms.SelectFromList(
+        '''Выберите csv-таблицу для экспорта''',
+        list_of_size_tables
+    )
+    selected_size_table = family_size_table_manager.GetSizeTable(selected_size_table)
+    print(selected_size_table) # Удалить
+    print(selected_size_table.AsValueString(1,1)) # Удалить
+
+    #Алгоритм перебора
+    # Размер таблицы:
+    columns = selected_size_table.NumberOfColumns
+    rows = selected_size_table.NumberOfRows
+
 else:
     forms.Alert('Это документ', exit=True)
