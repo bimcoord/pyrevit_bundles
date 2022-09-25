8551933ff6791a00802132a7028032915b4d1cdc
bogdan <bogdan.bimcoord@gmail.com>
2022-09-25 15:13:14 +0300

    25/09 15/13

--- bimcoord.extension/bimcoord_github.tab/testing.panel/export_csv.pushbutton/script.py
+++ bimcoord.extension/bimcoord_github.tab/testing.panel/export_csv.pushbutton/script.py
@@ -4,6 +4,7 @@ __doc__ = '''Define later in bundle.yaml'''
 # Template
 from Autodesk.Revit import DB
 from rpw.ui import forms
+import System
 
 doc = __revit__.ActiveUIDocument.Document
 
@@ -18,30 +19,31 @@ if doc.IsFamilyDocument:
         '''Выберите csv-таблицу для экспорта''',
         list_of_size_tables
     )
-    selected_size_table = family_size_table_manager.GetSizeTable(selected_size_table)
+    selected_size_table_name = selected_size_table
+    selected_size_table = family_size_table_manager.GetSizeTable(selected_size_table_name)
 
-    #Алгоритм перебора
-    # Размер таблицы:
-    columns = selected_size_table.NumberOfColumns
-    rows = selected_size_table.NumberOfRows
-    # Перебор
+    rows, columns = selected_size_table.NumberOfRows, selected_size_table.NumberOfColumns # Размер таблицы
+    # Алгоритм перебора
     def foo():
         _ret = ''
         for column in range(columns):
             _ret += ''.join([selected_size_table.AsValueString(row, column).ToString(), ';'])
+        _ret = _ret[:-1]
         return _ret
 
     returned_string = ''
     for row in range(rows):
         returned_string += ''.join([foo(),'\n'])
+    returned_string = returned_string[:-1]
+    
+    # Получение шапки таблица:
+    print(selected_size_table.GetColumnHeader(1).Name)
 
-    print(returned_string)
-
-    # Запись: 
-
-    # Получение шапки таблица. Также под вопросом 
-    # print(columns,rows)
-    # print(selected_size_table.GetColumnHeader(2).GetUnitTypeId()) # Получение информации о типе под вопросом
+    # Запись:
+    # path = ''.join([forms.select_folder(), '\\', selected_size_table_name, '.csv'])
+    # sw = System.IO.StreamWriter(path)
+    # sw.Write(returned_string)
+    # sw.Close()
 
 else:
     forms.Alert('Это документ', exit=True)
